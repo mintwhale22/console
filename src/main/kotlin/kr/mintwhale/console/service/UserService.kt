789e77e7629d741aab3b522488bcafe1c566ca5a
mintwhale <104501235+mintwhale22@users.noreply.github.com>
2022-06-01 13:56:37 +0900

    db설정

--- src/main/kotlin/kr/mintwhale/console/service/UserService.kt
+++ src/main/kotlin/kr/mintwhale/console/service/UserService.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.lang.reflect.Member
 
 @Service
 class UserService {
@@ -13,6 +14,8 @@ class UserService {
     @Autowired
     lateinit var memberMapper: MemberMapper
 
-    
+    fun login(data: Member) {
+
+    }
 
 }
